696c07949556695ac1a7f6f78ad8284e67d0bcae
VikaIz <vika.izmalkova.05@mail.ru>
2025-11-05 12:51:19 +0300

    Update constant.php

--- lab1/constant.php
+++ lab1/constant.php
@@ -1,49 +1,30 @@
 <?php
-/*
-ЗАДАНИЕ 1
-- Создайте константу и присвойте ей значение.
-*/
-define("SITE_NAME", "Мой сайт");
-const VERSION = "1.0.0";
+// ЗАДАНИЕ 1
+define('CONSTANT', 'Константа');
 ?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>Константы</title>
 </head>
 <body>
 	<h1>Константы</h1>
 	<?php
-	/*
-	ЗАДАНИЕ 2
-	- Проверьте, существует ли константа, которую Вы хотите использовать.
-	- Выведите значение созданной константы.
-	*/
-	if (defined("SITE_NAME")) {
-		echo "Название сайта: " . SITE_NAME . "<br>";
+	// ЗАДАНИЕ 2
+	if (defined('CONSTANT')) {
+	    echo 'Константа CONSTANT существует: ' . CONSTANT . '<br>';
 	} else {
-		echo "Константа SITE_NAME не определена<br>";
+	    echo 'Константа CONSTANT не существует.<br>';
 	}
 	
-	if (defined("VERSION")) {
-		echo "Версия: " . VERSION . "<br>";
-	} else {
-		echo "Константа VERSION не определена<br>";
-	}
-	
-	/*
-	ЗАДАНИЕ 3
-	- Используя предопределённые в ядре константы выведите текущую версию PHP.
-	- Используя магические константы выведите директорию скрипта.
-	*/
-	echo "Текущая версия PHP: " . PHP_VERSION . "<br>";
-	echo "Операционная система: " . PHP_OS . "<br>";
-	echo "Директория скрипта: " . __DIR__ . "<br>";
-	echo "Полный путь к файлу: " . __FILE__ . "<br>";
-	echo "Текущая строка: " . __LINE__ . "<br>";
+	// ЗАДАНИЕ 3
+	// Версия PHP
+	echo 'Текущая версия PHP: ' . PHP_VERSION . '<br>';
+	// Директория скрипта
+	echo 'Директория скрипта: ' . __DIR__ . '<br>';
 	?>
 </body>
 </html>
